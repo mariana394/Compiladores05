ca57f0e7a23528d2e0f7dffd8fa5049f4a72f3fc
Mario Juárez Castillo <mariojc29@gmail.com>
2023-04-25 15:30:01 -0600

    Cubo semantico

--- Fase_01/Func_Vars_Dictionary.py
+++ Fase_01/Func_Vars_Dictionary.py
@@ -2,13 +2,13 @@
 # # DATALOR: DICTIONARIES - VARIABLES TABLE
 # # Mariana Favarony Avila -A01704671
 # # Mario Juarez - A01411049
-# # PASER.... 
+# #  
 # # ------------------------------------------------------------
 
 # class Dictionaries:
         
 #         def __init__(self):
-#                 # Tabla de funciones 
-#                # self.func_table(
-#                      #   {0:}
+#                 #Tabla de funciones 
+#                 self.func_table(
+#                        {0:}
 #                 )
